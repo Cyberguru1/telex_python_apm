a2363ee7097201b42c1430eedcbf7701536491d4
cyberguru <hamzacypher34@gmail.com>
2024-11-13 01:44:17 -0600

    chore: fix readme

--- setup.py
+++ setup.py
@@ -17,4 +17,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
 )
